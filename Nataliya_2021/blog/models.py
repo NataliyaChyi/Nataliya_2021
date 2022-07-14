0e1bbe2b9f43a49c3ab414a57f2a9074fe7ff838
Наталия <105249744+NataliyaChyi@users.noreply.github.com>
2022-07-14 21:48:32 +0300

    add category, GET, POST

--- Nataliya_2021/blog/models.py
+++ Nataliya_2021/blog/models.py
@@ -4,12 +4,18 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=30)
 
-
 class Post(models.Model):
     title = models.CharField(max_length=250)
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     last_modifield = models.DateTimeField(auto_now=True)
     categories = models.ManyToManyField('Category', related_name='post')
+
+class Comment(models.Model):
+    author = models.CharField(max_length=100)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True, db_index=True)
+    post = models.ForeignKey('Post', on_delete=models.CASCADE)
+
 
 # Create your models here.
